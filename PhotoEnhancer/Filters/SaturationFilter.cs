2b9a866a2fadaff57a23c212847f47ce3f5c1255
hammerhearted <nataran2001@mail.ru>
2020-10-22 22:59:53 +0500

    Выполнено домашнее задание по созданию фильтра

---
+++ PhotoEnhancer/Filters/SaturationFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    class SaturationFilter: PixelFilter
+    {
+        public SaturationFilter() : base(new SaturationParameters()) { }
+        public override string ToString()
+        {
+            return "Насыщенность";
+        }
+        public override Pixel ProcessPixel(Pixel originalPixel,
+            IParameters parameters)
+        {
+            var origH = Convertors.GetPixelHue(originalPixel);
+            var origS = Convertors.GetPixelSaturation(originalPixel);
+            var origL = Convertors.GetPixelLightness(originalPixel);
+
+            var newS =origS * (parameters as SaturationParameters).Coefficient;
+            if (newS < 1 & newS>0)
+                return Convertors.HSL2Pixel(origH, newS, origL);
+            else
+                return Convertors.HSL2Pixel(origH, origS, origL);
+
+
+        }
+    }
+}
---
+++ PhotoEnhancer/Filters/SaturationParameters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace PhotoEnhancer
+{
+    public class SaturationParameters : IParameters
+    {
+        public double Coefficient { get; set; }
+
+        public ParameterInfo[] GetDescription()
+        {
+            return new[]
+            {
+                new ParameterInfo() {
+                    Name = "Коэффициент увеличения/уменьшения насыщенности",
+                    MinValue = 0,
+                    MaxValue = 10,
+                    DefailtValue = 1,
+                    Increment = 0.05
+                    }
+            };
+        }
+
+        public void SetValues(double[] values)
+        {
+            Coefficient = values[0];
+        }
+    }
+}
+
+
